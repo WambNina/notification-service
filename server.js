a4253f1bd1467ddf36aeb7e7c5e5fc18ecb5c6d6
WambNina <ninawamba12@gmail.com>
2026-05-13 15:47:30 +0200

    fix: swagger ui

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./docs/swagger');
+
+const swaggerOptions = {
+  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.10.3/swagger-ui.min.css',
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.10.3/swagger-ui-bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.10.3/swagger-ui-standalone-preset.min.js'
+  ]
+};
 const messageRoutes = require('./routes/messageRoutes');
 const fs = require('fs');
 const path = require('path');
@@ -26,7 +34,7 @@ app.use((req, res, next) => {
 });
 
 // Swagger Documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerOptions));
 
 // API Routes
 app.use('/api', messageRoutes);
